1ec1c5257d36c32bd6f666e0a6a3bd51194ec5db
Yasser <yasserreslan@Yassers-MacBook-Pro.local>
2023-11-09 22:39:25 +0200

    push

--- aws.tf
+++ aws.tf
@@ -47,224 +47,87 @@ resource "aws_subnet" "subnet_a" {
   )
 }
 
-# # Define an Auto Scaling Group
-# resource "aws_launch_configuration" "my_launch_config" {
-#   name_prefix = "my-launch-config-"
-#   image_id = "ami-12345678" # Specify your desired EC2 AMI
-#   instance_type = "t2.micro" # Change this as needed
+# Create a security group for Fargate services
+resource "aws_security_group" "fargate_sg" {
+  name_prefix = "my-fargate-sg-"
+  vpc_id      = aws_vpc.my_vpc.id
 
-# user_data = <<-EOF
-#               #!/bin/bash
-#               echo ECS_CLUSTER=${aws_ecs_cluster.my_cluster.name} >> /etc/ecs/ecs.config
-          
-#               # Install the SSM agent
-#               yum install -y https://s3.eu-central-1.amazonaws.com/amazon-ssm-eu-central-1/latest/linux_amd64/amazon-ssm-agent.rpm
-#               systemctl enable amazon-ssm-agent
-#               systemctl start amazon-ssm-agent
-          
-#               # Install the ECS agent
-#               amazon-linux-extras install -y ecs
-#               systemctl enable ecs
-#               systemctl start ecs
-#             EOF
+  # Ingress rule to allow traffic from ALB to Fargate services
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id] # Allow traffic from ALB security group
+  }
 
-#   security_groups = [aws_security_group.my_security_group.id]
-#   key_name = "my-keypair" # Replace with your SSH key name if needed
-# }
+  # Egress rule to allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# resource "aws_security_group" "my_security_group" {
-#   name_prefix = "my-sg-"
+# IAM execution role and policy remain the same
 
-#   # Ingress rule to allow traffic from alb to the ec2
-#   ingress {
-#     from_port   = 80 # Use the port used by your ALB listener
-#     to_port     = 80 # Use the same port as from_port
-#     protocol    = "tcp"
-#     cidr_blocks = [aws_security_group.alb_sg.id] # Allow traffic from alb
-#   }
-#   # Egress rule to allow all outbound traffic
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-  
-# }
+# Define the Fargate-compatible task definition
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family                   = "my-task-family"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  cpu                      = "256"  # Adjust based on your needs
+  memory                   = "512"  # Adjust based on your needs
 
-# resource "aws_autoscaling_group" "my_auto_scaling_group" {
-#   name_prefix = "my-auto-scaling-group-"
-#   launch_configuration = aws_launch_configuration.my_launch_config.name
-#   min_size = 1
-#   max_size = 2
-#   desired_capacity = 1
-#   vpc_zone_identifier = aws_subnet.subnet_a[*].id
-# }
+  network_mode = "awsvpc" # Required for Fargate
 
-# # Create an IAM execution role for ECS tasks and attach the AmazonECSTaskExecutionRole policy
-# resource "aws_iam_role" "ecs_execution_role" {
-#   name = "my-ecs-execution-role"
+  container_definitions = jsonencode([{
+    name  = "ghost"
+    image = "${aws_ecr_repository.my_ecr_repository.repository_url}:latest"
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Action = "sts:AssumeRole",
-#       Effect = "Allow",
-#       Principal = {
-#         Service = "ecs-tasks.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
+# ALB and ALB security group remain the same
 
-# # Attach an IAM policy that provides ECR permissions to the execution role
-# resource "aws_iam_policy" "ecs_execution_policy" {
-#   name        = "my-ecs-execution-policy"
-#   description = "ECR Access Policy for ECS Execution Role"
+# Define the Fargate ECS service
+resource "aws_ecs_service" "my_service" {
+  name            = "my-ecs-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task_definition.arn
+  launch_type     = "FARGATE"
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Action = [
-#         "ecr:GetDownloadUrlForLayer",
-#         "ecr:GetRepositoryPolicy",
-#         "ecr:BatchCheckLayerAvailability",
-#         "ecr:GetAuthorizationToken",
-#         "ecr:GetImageManifest",
-#         "ecr:InitiateLayerUpload",
-#         "ecr:UploadLayerPart",
-#         "ecr:CompleteLayerUpload",
-#         "ecr:PutImage",
-#       ],
-#       Effect   = "Allow",
-#       Resource = "*",
-#     }]
-#   })
-# }
+  network_configuration {
+    subnets = aws_subnet.subnet_a[*].id
+    security_groups = [aws_security_group.fargate_sg.id]
+  }
 
-# # Attach the ECR policy to the execution role
-# resource "aws_iam_policy_attachment" "ecs_execution_role_ecr_attachment" {
-#   name       = "ecs_execution_role_ecr_attachment"
-#   roles      = [aws_iam_role.ecs_execution_role.name]
-#   policy_arn = aws_iam_policy.ecs_execution_policy.arn
-# }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+    container_name   = "my-container"
+    container_port   = 80
+  }
 
-# resource "aws_iam_role" "ecs_task_role" {
-#   name = "my-ecs-task-role"
+  desired_count = 1
+}
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole",
-#         Effect = "Allow",
-#         Principal = {
-#           Service = "ecs-tasks.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+# Define a target group for the load balancer
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
 
-# # Define the task definition referencing the execution role and task role
-# resource "aws_ecs_task_definition" "my_task_definition" {
-#   family                   = "my-task-family"
-#   network_mode             = "bridge"
-#   requires_compatibilities = ["EC2"]
-#   execution_role_arn        = aws_iam_role.ecs_execution_role.arn # Reference the execution role ARN
-#   task_role_arn            = aws_iam_role.ecs_task_role.arn # Reference the task role ARN
-
-#   container_definitions = jsonencode([{
-#     name  = "my-container"
-#     image = "${aws_ecr_repository.my_ecr_repository.repository_url}:latest"
-#     portMappings = [{
-#       containerPort = 80
-#       hostPort      = 80
-#     }]
-#   }])
-# }
-
-# # Create a security group for the ALB
-# resource "aws_security_group" "alb_sg" {
-#   name        = "my-alb-sg"
-#   description = "Security Group for Application Load Balancer"
-#   vpc_id      = aws_vpc.my_vpc.id # Use your VPC ID
-
-#   # Ingress rule to allow traffic from internet to the alb
-#   ingress {
-#     from_port   = 80 # Use the port used by your ALB listener
-#     to_port     = 80 # Use the same port as from_port
-#     protocol    = "tcp"
-#     cidr_blocks = [0.0.0.0/32] # Allow traffic from everywhere
-#   }
-#   # Egress rule to allow all outbound traffic
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-# }
-
-# # Define an Application Load Balancer
-# resource "aws_lb" "my_load_balancer" {
-#   name               = "my-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   subnets            = aws_subnet.subnet_a[*].id
-#   enable_deletion_protection = false
-#   security_groups = [aws_security_group.alb_sg.id] # Attach the ALB security group
-# }
-
-# # Define a listener for the load balancer
-# resource "aws_lb_listener" "my_listener" {
-#   load_balancer_arn = aws_lb.my_load_balancer.arn
-#   port              = 80
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type             = "fixed-response"
-#     fixed_response {
-#       content_type = "text/plain"
-#       status_code  = "200"
-#       content      = "OK"
-#     }
-#   }
-# }
-
-# # Define an ECS service
-# resource "aws_ecs_service" "my_service" {
-#   name            = "my-ecs-service"
-#   cluster         = aws_ecs_cluster.my_cluster.id
-#   task_definition = aws_ecs_task_definition.my_task_definition.arn
-#   launch_type     = "EC2"
-
-#   network_configuration {
-#     subnets = aws_subnet.subnet_a[*].id
-#     security_groups = [aws_security_group.my_security_group.id]
-#   }
-
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.my_target_group.arn
-#     container_name   = "my-container"
-#     container_port   = 80
-#   }
-# }
-
-# # Define a target group for the load balancer
-# resource "aws_lb_target_group" "my_target_group" {
-#   name     = "my-target-group"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.my_vpc.id
-
-#   health_check {
-#     path                = "/"
-#     interval            = 30
-#     timeout             = 5
-#     healthy_threshold   = 2
-#     unhealthy_threshold = 2
-#     matcher             = "200"
-#   }
-# }
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
