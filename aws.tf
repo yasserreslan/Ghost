7e18c9e7f8adb8f86596533d1978ebcc277a9769
Yasser <yasserreslan@Yassers-MacBook-Pro.local>
2023-11-09 23:26:34 +0200

    push

--- aws.tf
+++ aws.tf
@@ -181,6 +181,26 @@ resource "aws_iam_role" "ecs_task_role" {
   })
 }
 
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.subnet_a[*].id
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
+}
 
 # Define the Fargate ECS service
 resource "aws_ecs_service" "my_service" {
@@ -210,7 +230,7 @@ resource "aws_lb_target_group" "my_target_group" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc.id
   target_type = "ip"
-  
+
   health_check {
     path                = "/"
     interval            = 30
@@ -220,3 +240,4 @@ resource "aws_lb_target_group" "my_target_group" {
     matcher             = "200"
   }
 }
+
