739036ef87a5a24f67f6ae63660a18c1293290a5
Yasser <yasserreslan@Yassers-MacBook-Pro.local>
2023-11-09 23:02:02 +0200

    push

--- aws.tf
+++ aws.tf
@@ -209,7 +209,8 @@ resource "aws_lb_target_group" "my_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc.id
-
+  target_type = "ip"
+  
   health_check {
     path                = "/"
     interval            = 30
