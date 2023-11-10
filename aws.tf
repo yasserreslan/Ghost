dbaad57067c9015789c07bd5b20c53c58a54c583
Yasser <yasserreslan@Yassers-MacBook-Pro.local>
2023-11-10 14:19:03 +0200

    new push

--- aws.tf
+++ aws.tf
@@ -90,6 +90,27 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.subnet_a[*].id
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
+}
+
 
 
 # IAM execution role and policy
