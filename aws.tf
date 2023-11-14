de9d3de716f36a30ed2b630944c2f2bcce9f75f8
Yasser <yasserreslan@Yassers-MacBook-Pro.local>
2023-11-14 16:13:22 +0200

    push

--- aws.tf
+++ aws.tf
@@ -106,9 +106,9 @@ resource "aws_security_group" "fargate_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
